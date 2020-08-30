dc4bddd1222a3cdfc7ca73f82b6744dca871a2b7
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-08-31 01:06:38 +0700

    Add files via upload

---
+++ max_min_keyword_java/w1.java
@@ -0,0 +1,13 @@
+public class w1 {
+	public static void main(String args[]){
+
+	int w1=Integer.parseInt(args[0]);
+	int w2=Integer.parseInt(args[1]);
+	int w3=Integer.parseInt(args[2]);
+	int w4=Integer.parseInt(args[3]);
+	System.out.println( "Plus = " + (w1+ w2 + w3 + w4));
+	System.out.println( "Minus = " + (w1 - w2 - w3- w4));
+	System.out.println( "Multiplied = " + (w1 * w2 * w3 * w4));
+	System.out.println( "Divide = " + (w1 / w2 / w3 / w4));		
+	}
+}
---
+++ max_min_keyword_java/w2.java
@@ -0,0 +1,21 @@
+class w2 {
+	public static void main(String args[]){
+ 
+	String a1 = "Wiw";
+	byte a2 = -20;
+	short a3 = 23;
+	char a4 =  'W';
+	int a5 =  1234;
+	long a6 = 678l;
+	float a7 = 678.87f;
+	double a8 = 234234.234;
+	System.out.println(a1 + a6); 
+	System.out.println(a7 - a8);  
+	System.out.println(a4 * a7);
+	System.out.println(a2 / a3);
+	System.out.println(a6 + a1);
+	System.out.println(a8 - a4);
+	System.out.println(a2 * a2);
+	System.out.println(a5 / a5);
+	}
+}
