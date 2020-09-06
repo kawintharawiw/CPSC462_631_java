6639fc4264dbde1c10acadf048c4576a38fdf2f2
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-09-06 23:51:39 +0700

    Update one.java

--- java_bufferedreader/one.java
+++ java_bufferedreader/one.java
@@ -4,10 +4,10 @@ class one {
     BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
     int i[] = new int[5];
     int plus = 0, Minus = 0, Multi = 0, Divide = 0;
-    for (int j=0; j<5; j++) {
+    for (int k=0; k<5; k++) {
     i[j] = Integer.parseInt(stdin.readLine());
     }
-    for (int j=0; j<5; j++) {
+    for (int k=0; k<5; k++) {
       plus = i[0] + i[1] + i[2] + i[3] + i[4];
       Minus = i[0] - i[1] - i[2] - i[3] - i[4];
       Multi = i[0] * i[1] * i[2] * i[3] * i[4];
