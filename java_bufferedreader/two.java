4a8b7cde50c42fc6c53b5426814dc5c396d652c7
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-09-06 20:19:48 +0700

    Update two.java

--- java_bufferedreader/two.java
+++ java_bufferedreader/two.java
@@ -5,12 +5,12 @@ public class two {
     int a[] = new int[5];
     int b[] = new int[5];
     for (int i=0; i<5; i++) {
-      a[i] = Integer.parseInt(stdin.readLine());
-      b[i] = Integer.parseInt(stdin.readLine());
+    a[i] = Integer.parseInt(stdin.readLine());
+    b[i] = Integer.parseInt(stdin.readLine());
     }
     try {
       for (int i=0; i<6; i++) {
-        System.out.println("result = " + a[i] / b[i]);
+      System.out.println("result = " + a[i] / b[i]);
       }
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println(e);
