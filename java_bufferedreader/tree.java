cf52c15d2962e890f29ddd3b511e258b80e5cb25
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-09-06 20:21:58 +0700

    Update tree.java

--- java_bufferedreader/tree.java
+++ java_bufferedreader/tree.java
@@ -5,14 +5,14 @@ class tree {
     int a[] = new int[5];
     int b[] = new int[5];
     for (int i=0; i<5; i++) {
-      a[i] = Integer.parseInt(stdin.readLine());
-      b[i] = Integer.parseInt(stdin.readLine());
+    a[i] = Integer.parseInt(stdin.readLine());
+    b[i] = Integer.parseInt(stdin.readLine());
     }
     for (int i=0; i<5; i++) {
-      try {
-        System.out.println("result = " + a[i] / b[i]);
-      } catch (ArithmeticException e) {
-        System.out.println(e);
+    try {
+    System.out.println("result = " + a[i] / b[i]);
+    } catch (ArithmeticException e) {
+      System.out.println(e);
       }
     }
   }
