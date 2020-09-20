1f51dc5504ec87cb1240eb5c394f72d3ad7cd02c
Kawintharawiw <45837621+kawintharawiw@users.noreply.github.com>
2020-09-20 21:25:42 +0700

    Update abc.java

--- array two-dimensional/abc.java
+++ array two-dimensional/abc.java
@@ -3,20 +3,24 @@ class aa{
   static String data[][] = new String[3][3];
     public static void aaa() throws IOException{
     BufferedReader numid = new BufferedReader (new InputStreamReader(System.in));
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 3; i++){ 
+	    
       for(int k = 0; k < 3; k++){
         if(i == 0){
           System.out.print("enter ID : ");
           data[i][k] = numid.readLine(); 
 	  }
+	      
 	  else if(i == 1){
 	  System.out.print("enter Name : ");
 	  data[i][k] = numid.readLine(); 
 	  }
+	      
 	  else if(i == 2){
           System.out.print("enter Salary : ");
           data[i][k] = numid.readLine(); 
           } 
+	      
           else{
           System.out.print("Error");
 	  }            
@@ -53,19 +57,20 @@ class bb extends aa{
             }
             System.out.println("|");
         }
-        System.out.println("-------------------------------------");
+        System.out.println("***************************************");
+	    
         System.out.println("Total = " + (sum)+ " $");
         System.out.println("Social Security = " + (500)+ " $");
         System.out.println("Tax 7% = " + (sum * 0.07)+ " $");
         System.out.println("Net Salary = " + ((sum*0.93)-500) + " $");
-        System.out.println("-------------------------------------");
+	    
+        System.out.println("***************************************");
         } catch (Exception e) {           
-        }     
-    }
+      }     
+   }
 }
 class cc extends bb {
     public static void main(String args[]) {
-
-        bbb();
-    }
+      bbb();
+      }
 }
